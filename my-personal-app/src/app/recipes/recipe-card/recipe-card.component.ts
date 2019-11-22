cd1079349b3e28441460a1790e1c5980a6e8b7f9
tia2019 <tiaholmes2015@gmail.com>
2019-11-22 05:40:32 -0500

    show ingredients

--- my-personal-app/src/app/recipes/recipe-card/recipe-card.component.ts
+++ my-personal-app/src/app/recipes/recipe-card/recipe-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { Ingredient } from 'src/app/ingredients/ingredient.model';
 
 @Component({
   selector: 'app-recipe-card',
@@ -10,13 +11,13 @@ import { Recipe } from '../recipe.model';
 export class RecipeCardComponent implements OnInit {
   @Input()
   recipe: Recipe;
-
+  ingredientsArray: Ingredient[];
   constructor() {
 
   }
 
   ngOnInit() {
-
+  this.ingredientsArray = this.recipe.ingredients;
   }
 
 }
